6f3f512bf645b2508fdbfd97ff263d561f988d4e
Sefik Ilkin Serengil <serengil@gmail.com>
2017-07-11 10:30:02 +0300

    blocks activated

--- dnnregressor.py
+++ dnnregressor.py
@@ -2,12 +2,13 @@ import tensorflow as tf
 import numpy as np
 
 input = np.loadtxt("sine.csv", dtype='f', delimiter=',')
-
 #print(input)
 
 row = input.shape[0]
 col = input.shape[1]
 
+#----------------------------
+#attributes and labels
 attributes = [[0 for i in range(col-1)] for j in range(row)]
 labels = []
 for i in range(row):
@@ -25,17 +26,21 @@ target = np.array(labels, 'float32')
 
 #----------------------------
 
+#nn learning parameters 
 learningRate = 0.1
 epoch = 10000
 
+#----------------------------
+
 feature_columns = [tf.contrib.layers.real_valued_column("")]
 
+#neural network model
 regressor = tf.contrib.learn.DNNRegressor(
 	feature_columns = feature_columns
-	, hidden_units = [4]
+	, hidden_units = [4] #a hidden layer consisting of 4 nodes
 	, optimizer = tf.train.GradientDescentOptimizer(learningRate)
 	, activation_fn = tf.nn.sigmoid
-	, model_dir = "model"
+	, model_dir = "model" #model will be stored in this folder
 	, config = tf.contrib.learn.RunConfig(save_checkpoints_secs = 1)
 )
 
@@ -50,20 +55,26 @@ def test_set():
 
 predictions = regressor.predict_scores(input_fn = test_set)
 
-"""
+#--------------------------
+#dumping predictions and actual sets
+
 index = 0
 for i in predictions:
 	print("actual: ", target[index],", predic: ", i)
 	index = index + 1
-"""
 
 #--------------------------
-"""
+
+#model performance
 eva = regressor.evaluate(data, target)
 print("MSE: ", eva["loss"])
-"""
 
 #--------------------------
+
+#visualizing predictions and actuals in TensorBoard
+
+#predictions have to be restored for processing
+predictions = regressor.predict_scores(input_fn = test_set)
 
 actuals = labels
 forecasts = list(predictions)
