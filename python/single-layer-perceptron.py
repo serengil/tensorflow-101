71863bdf84a2f240b224f80896d8aa68fdd446f8
Sefik Ilkin Serengil <serengil@gmail.com>
2018-08-08 09:34:45 +0300

    more clear code written

--- python/single-layer-perceptron.py
+++ python/single-layer-perceptron.py
@@ -2,59 +2,52 @@ import numpy as np
 
 operator = 'and'
 
-atributes = [
-	[0, 0]
-	, [0, 1]
-	, [1, 0]
-	, [1, 1]
-]
+#----------------
+
+atributes = np.array([ [0, 0], [0, 1], [1, 0], [1, 1]])
 
 if operator == 'and':
-	labels = [0, 0, 0, 1]
+	labels = np.array([0, 0, 0, 1])
 elif operator == 'or':
-	labels = [0, 1, 1, 1]
-
-w = [-1, -1, -1] #initial values for weights
+	labels = np.array([0, 1, 1, 1])
+elif operator == 'xor':
+	labels = np.array([0, 1, 1, 0])
 
 #----------------
+
+w = [+9, +9] #initial random values for weights
+
 threshold = 5
 alpha = 0.5 #learning rate
-epoch = 1000
-bias = 1
+epoch = 1000 #learning time
 #----------------
 
 print("learning rate: ", alpha,", threshold: ", threshold)
 
-data = np.array(atributes, 'int64')
-target = np.array(labels, 'int64')
-
 for i in range(0, epoch):
 	print("epoch ", i+1)
-	index = 0
-	global_delta = 0
+	global_delta = 0 #this variable is used to terminate the for loop if learning completed in early epoch
 	for j in range(len(atributes)):
 		
-		actual = labels[index]
+		actual = labels[j]
 		
-		sum = data[index][0]*w[0] + data[index][1]*w[1] + bias * w[2]
+		sum = atributes[j][0]*w[0] + atributes[j][1]*w[1]
 		
-		if sum > threshold:
+		if sum > threshold: #then fire
 			predicted = 1
-		else:
+		else: #do not fire
 			predicted = 0
 		
 		delta = actual - predicted
 		global_delta = global_delta + abs(delta)
 		
-		for k in range(0, 3):
+		#update weights with respect to the error
+		for k in range(0, 2):
 			w[k] = w[k] + delta * alpha
 				
-		print(data[index][0]," ", operator, " ", data[index][1], " -> actual: ", actual, ", predicted: ", predicted, " (w: ",w[0],")")
-		
-		index = index + 1
+		print(atributes[j][0]," ", operator, " ", atributes[j][1], " -> actual: ", actual, ", predicted: ", predicted, " (w: ",w[0],")")
 		
 	if global_delta == 0:
 		break
 	
 	print("------------------------------")
-	
